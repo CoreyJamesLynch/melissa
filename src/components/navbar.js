99993a1ca348170dcf84c6dd3951a0189b73e126
bearinawolfpack <bearinawolfpack@gmail.com>
2021-09-14 14:34:41 -0400

    Create navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='/'>Melissa's Mindset</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'>
+            </Nav>
+            <Nav>
+              <Nav.Link href='/plans'>Plans</Nav.Link>
+              <Nav.Link eventKey={2} href='/about'>About</Nav.Link>
+              <Nav.Link eventKey={3} href='/contact'>
+                Commit
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default NavigationBar;
