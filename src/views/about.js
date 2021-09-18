2d0d70adfe2c0649732f6dd22abbfe32c597ed05
bearinawolfpack <bearinawolfpack@gmail.com>
2021-09-18 01:34:47 -0400

    Make stateless component into pure component

--- src/views/about.js
+++ src/views/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 
-class About extends React.Component {
+class About extends React.PureComponent {
   render() {
     return (
       <>
