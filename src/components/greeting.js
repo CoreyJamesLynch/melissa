d510a6edf1ea07f07c37c5c19693e736ad37b433
bearinawolfpack <bearinawolfpack@gmail.com>
2021-09-18 01:41:09 -0400

    Create greeting component as section 1 of home page

---
+++ src/components/greeting.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+class Greeting extends React.PureComponent {
+  render() {
+    return (
+      <Container>
+        <h1>This is Section 1</h1>
+      </Container>
+    );
+  }
+}
+
+export default Greeting;
