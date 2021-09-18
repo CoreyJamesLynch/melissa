61dd3b34c696451d03fc47c5a464ab61b5ab57e2
bearinawolfpack <bearinawolfpack@gmail.com>
2021-09-18 01:29:47 -0400

    Move router up in config order

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import NavBar from './components/navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './components/navigation_bar';
 import Plans from './views/plans';
 import About from './views/about';
 import Contact from './views/contact';
 import Home from './views/home';
 
-function App() {
-  return (
-    <>
-      <NavBar />
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />¸
-          <Route path='/plans' component={Plans} />¸
-          <Route path='/about' component={About} />¸
-          <Route path='/contact' component={Contact} />¸
-          <Route component={Home} />
-        </Switch>
-      </Router>
-    </>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <>
+        <NavBar />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />¸
+            <Route path="/plans" component={Plans} />¸
+            <Route path="/about" component={About} />¸
+            <Route path="/contact" component={Contact} />¸
+            <Route component={Home} />
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
 
 export default App;
