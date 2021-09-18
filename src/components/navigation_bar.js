cebf75e2117d59e9b64350802a90932601936c77
bearinawolfpack <bearinawolfpack@gmail.com>
2021-09-18 01:33:54 -0400

    Remove unused import, escape ', empty elements are self closing

--- src/components/navigation_bar.js
+++ src/components/navigation_bar.js
@@ -4,18 +4,18 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 class NavigationBar extends React.PureComponent {
   render() {
     return (
-      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href='/'>Melissa's Mindset</Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='me-auto'></Nav>
+          <Navbar.Brand href="/">Melissa&apos;s Mindset</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto" />
             <Nav>
-              <Nav.Link href='/plans'>Plans</Nav.Link>
-              <Nav.Link eventKey={2} href='/about'>
+              <Nav.Link href="/plans">Plans</Nav.Link>
+              <Nav.Link eventKey={2} href="/about">
                 About
               </Nav.Link>
-              <Nav.Link eventKey={3} href='/contact'>
+              <Nav.Link eventKey={3} href="/contact">
                 Commit
               </Nav.Link>
             </Nav>
