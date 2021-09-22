31e661bfc572b748fb08518c5ff4cac1c356cb8a
bearinawolfpack <bearinawolfpack@gmail.com>
2021-09-21 21:19:38 -0400

    Build out prototype greeting section on home

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
+import Melissa from '../assets/melissa.jpeg';
 
-class Greeting extends React.PureComponent {
-  render() {
-    return (
+const Greeting = () => (
+  <>
+    <section className="bg-white text-dark p-5 text-center text-sm-start">
       <Container>
-        <h1>This is Section 1</h1>
+        <div className="d-sm-flex align-items-center justify-content-between">
+          <div>
+            <h1>Be Your Best Self</h1>
+            <p className="lead my-4">
+              I will help you improve your life and conquer your goals through
+              small changes to your diet. Schedule a consolation to take the
+              first step changing your life forever.
+            </p>
+            <Button variant="success" size="lg">
+              Commit
+            </Button>
+          </div>
+          <img
+            className="img-fluid w-50 d-none d-sm-block"
+            src={Melissa}
+            alt=""
+          />
+        </div>
       </Container>
-    );
-  }
-}
+    </section>
+  </>
+);
 
 export default Greeting;
