6a8ec66937731843f48fd56e7dda93a19598c9cd
bearinawolfpack <bearinawolfpack@gmail.com>
2021-09-21 21:18:59 -0400

    Remove unused container

--- src/views/home.js
+++ src/views/home.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import Greeting from '../components/greeting';
 
 class Home extends React.PureComponent {
   render() {
     return (
       <>
-        <Container>
-          <Greeting />
-        </Container>
+        <Greeting />
       </>
     );
   }
